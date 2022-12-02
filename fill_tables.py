34df8c024e4e84e889431a2af703b91b323afa36
Michael B. Streed <mstreed19@outlook.com>
2022-12-01 22:38:27 -0600

    started filling tabels

--- fill_tables.py
+++ fill_tables.py
@@ -8,4 +8,7 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 def fill_tables():
     # fill supplier table
-    supplier = "INSERT INTO" supplier (Supplier_ID, Name, Street_Address, Street_Address2, zip, Contact_First_Name, Contact_Last_Name)
+    supplier = "INSERT INTO supplier (Supplier_ID, Name, Street_Address, Street_Address2, zip, Contact_First_Name, Contact_Last_Name, Phone_Number,Email_Address, order_method, active) VALUES (%S,%S,%S,%S,%S,%S,%S,%S,%S,%S,%S,%S)"
+    val = [
+        (1,'Southern Glazer', '2400 SW 145th AVE', 'Suite200', 33027, 'Angel', 'Garay', '305-627-1202', 'angel.garay@shws.com', 'online', 'these are details', 'y')
+    ]
