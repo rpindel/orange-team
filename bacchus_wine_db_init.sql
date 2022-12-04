0d3bdbcb5bbb8e2658748c5f3b683fe3f60b143b
rpindel <116233596+rpindel@users.noreply.github.com>
2022-12-03 22:43:49 -0600

    Update bacchus_wine_db_init.sql

--- bacchus_wine_db_init.sql
+++ bacchus_wine_db_init.sql
@@ -5,6 +5,7 @@
     Description: bacchus wine databse initialization SQL script
 */
 
+DROP DATABASE IF EXISTS bacchus_wine;
 CREATE DATABASE bacchus_wine;
 DROP USER IF EXISTS 'bacchus_user'@'localhost';
 CREATE USER 'bacchus_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'mysqltest';
