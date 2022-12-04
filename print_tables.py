c846c339461d9753faad54d8e29a472a80d9f414
AmandaSherman <aleigh.sherman@gmail.com>
2022-12-03 23:33:12 -0600

    Formatting trial

--- print_tables.py
+++ print_tables.py
@@ -10,11 +10,13 @@ def print_tables():
     )
 
     cursor = db.cursor()
-    
+
     cursor.execute("SELECT * FROM supplier;")
 
     results = cursor.fetchall()
 
+    print("\n***{}***".format(title))
+
     for tables in results:
-        print(tables)
+        print("Supplier ID: {}\nName: {}\nAddress: {}\nAddress2: {}\nZip code: {}\nFirst name: {}\nLast name: {}\nPhone: {}\nEmail: {}\nOrder Method: {}\nMisc: {}\nActive: {}\n".format(tables[0], tables[1], tables[2], tables[3], tables[4], tables[5], tables[6], tables[7], tables[8], tables[9], tables[10], tables[11], tables[12]))
         print("\n")
