c807b808d5d23656d334ceb48150f2f3fe76e5eb
AmandaSherman <aleigh.sherman@gmail.com>
2022-12-03 23:42:55 -0600

    arguments fix

--- print_tables.py
+++ print_tables.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def print_tables(cursor, title):
+def print_tables():
 
     db = mysql.connector.connect(
         user="bacchus_user",
@@ -15,7 +15,7 @@ def print_tables(cursor, title):
 
     results = cursor.fetchall()
 
-    print("\n***{}***".format(title))
+    print("\n***Suppliers***")
 
     for tables in results:
         print("Supplier ID: {}\nName: {}\nAddress: {}\nAddress2: {}\nZip code: {}\nFirst name: {}\nLast name: {}\nPhone: {}\nEmail: {}\nOrder Method: {}\nMisc: {}\nActive: {}\n".format(tables[0], tables[1], tables[2], tables[3], tables[4], tables[5], tables[6], tables[7], tables[8], tables[9], tables[10], tables[11], tables[12]))
