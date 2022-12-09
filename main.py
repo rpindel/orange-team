a4ae0aebc1b6807750b72f3090e6d8d90fb08ae2
AmandaSherman <aleigh.sherman@gmail.com>
2022-12-08 21:46:36 -0600

    just ideas?

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import create_tables
 import fill_tables
-import print_tables
-
+#import print_tables
+import sales
 # we make our functions in the other files and call them on this one
 # main.py needs to have the bacchus_wine_db_init.sql run first
 
@@ -9,4 +9,5 @@ import print_tables
 create_tables.create_tables()
 fill_tables.fill_tables()
 create_tables.foreign_key_constraints()
-print_tables.print_tables()  # tentative name for this function
+#print_tables.print_tables()  # tentative name for this function
+sales.sales()
